ca22fc824f6943c3c20cebc146ffd84b579bd121
Crudu Alex <alexcruducode@gmail.com>
2023-01-04 15:05:12 +0200

    asd

--- components/Introduction.js
+++ components/Introduction.js
@@ -22,7 +22,7 @@ export default function Introduction() {
   };
 
   const textReveal = {
-    hidden: { opacity: 0, y: "10%" },
+    hidden: { opacity: 0, y: "15%" },
     visible: {
       opacity: 1,
       y: 0,
